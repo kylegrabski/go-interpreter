e2341a0afec823c43ff01618b615f39345cc00b2
Kyle Grabski <kyle.grabski@gmail.com>
2024-04-28 13:05:27 -0700

    nextToken func

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -6,6 +6,8 @@
 
 package lexer
 
+import "monkey/token"
+
 type Lexer struct {
 	input        string
 	position     int  // current position in input (points to current char)
@@ -14,9 +16,9 @@ type Lexer struct {
 }
 
 func New(input string) *Lexer {
-	// l := &Lexer{input: input}
-	// return l
-	return &Lexer{input: input}
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
 }
 
 func (l *Lexer) readChar() {
@@ -27,4 +29,37 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
